2fd59a647db1970451a08013c23e5a8cc62470fc
RastAI <rastai.robot@gmail.com>
2025-11-15 11:34:59 -0300

    feat(npc): Inicio da edição do boneco

--- krisley.js
+++ krisley.js
@@ -14,7 +14,9 @@ class Krisley {
 
     //ações publicas
     lerMensagem(message){
-        return `Krisley diz: ${message}`;
+        if(!message) return "Trabalhando po"
+        if(message==="oi gordim") return "NÃO NÃO NÃO TA DOIDO PO PASSANDO MAL NAO NAO OFF GRUPO OFF OFF VITOR NAO OFF GRUPO"
+        return `Krisley responde: ${message}`;
     }
 
 }
